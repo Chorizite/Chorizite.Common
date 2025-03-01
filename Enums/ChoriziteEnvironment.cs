a1b369041f2cbc060c849986ece8eb9a813880c3
trevis <trevis@haxit.org>
2025-03-01 16:51:59 -0700

    Add ChoriziteEnvironment enum (#1)

---
+++ Enums/ChoriziteEnvironment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chorizite.Common.Enums {
+    [Flags]
+    public enum ChoriziteEnvironment : uint {
+        Unknown = 0x00000000,
+        Launcher = 0x00000001,
+        Client = 0x00000002,
+        DocGen = Launcher | Client
+    }
+}
